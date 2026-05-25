45bb01672c049b75a8d163401bee707882f2d9d5
Felipe Walker <felipewlk@hotmail.com>
2026-05-25 18:59:21 -0300

    Del first comments

--- src/app.js
+++ src/app.js
@@ -1,36 +1,3 @@
-// const readline = require('readline-sync');
-
-// function criarMenu(titulo, opcoes) {
-//     while (true) {
-//         console.log(`\n==============================`);
-//         console.log(`    ${titulo.toUpperCase()}    `);
-//         console.log(`==============================`);
-
-//         opcoes.forEach((opt, index) => {
-//             console.log(`${index + 1}. ${opt.texto}`);
-//         });
-//         console.log(`0. Sair`);
-//         console.log(`==============================`);
-
-//         const escolha = readline.question("Escolha uma opcao: ");
-
-//         if (escolha === '0') {
-//             console.log("Encerrando sistema...");
-//             break;
-//         }
-
-//         const indice = parseInt(escolha) - 1;
-//         const opcaoSelecionada = opcoes[indice];
-
-//         if (opcaoSelecionada && typeof opcaoSelecionada.acao === 'function') {
-//             opcaoSelecionada.acao(readline);
-//         } else {
-//             console.log("⚠️ Opcao invalida!");
-//         }
-//     }
-// }
-
-// module.exports = { criarMenu };
 
 import readline from 'readline-sync';
 
@@ -83,7 +50,7 @@ function iniciarTotem() {
     }
 
 
-    // Adição PROBLEMA CARDÍACO
+    // PROBLEMA CARDÍACO
 
     let cardiaco = readline.question("Voce tem problemas cardiacos? (sim/nao): ");
     let temProblema = (cardiaco === 'sim' || cardiaco === 's');
